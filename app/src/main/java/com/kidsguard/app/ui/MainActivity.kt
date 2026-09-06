54cd2c09ce58c03642c3778a62087c1f03fd4044
yongyong2088 <hehongy@sohu.com>
2026-09-06 11:03:06 +0800

    3.3

--- app/src/main/java/com/kidsguard/app/ui/MainActivity.kt
+++ app/src/main/java/com/kidsguard/app/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kidsguard.app.ui
 import android.content.Intent
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
 import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.ImageView
